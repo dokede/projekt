ee5f12003a07279b0275e1cb4b8502454a4f610e
dokede <55347069+dokede@users.noreply.github.com>
2024-10-25 15:01:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')  # Renderuje plik index.html
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Importujemy `os`, aby pobrać numer portu z zmiennej środowiskowej
+    import os
+    
+    # Pobieramy port z systemu, domyślnie ustawiamy na 5000 (na wypadek, gdyby zmienna nie była ustawiona)
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Uruchamiamy aplikację na podanym porcie
+    app.run(host="0.0.0.0", port=port, debug=True)
